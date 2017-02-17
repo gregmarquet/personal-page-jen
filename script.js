43d74f5ebf7ce7b5978afa82515fbfa83c807ed4
gregmarquet <marquet.greg@gmail.com>
2017-02-16 23:57:26 -0500

    add jquery to fade title-container when scroll

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
+  var height = $(window).height();
   $('.title-container').css({
-    'transform': 'translate(0px, '+ wScroll / 2.3 +'%)'
+    'transform': 'translate(0px, '+ wScroll / 2.3 +'%)',
+    'opacity': (((height - 150) - wScroll) / height)
   });
+  
 
 });
+
