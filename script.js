32ab19a2689e7b728475073d29702cc5a68ef39a
gregmarquet <marquet.greg@gmail.com>
2017-02-18 14:35:16 -0500

    add js to fade other pre-block h1

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
   var height = $(window).height();
 
-  var offsetTop = $('.pre-block-1').offset().top;
-  var preBlockHeight = $('.pre-block-1').height();
+  var skillsPreBlockOffsetTop = $('.pre-block-1').offset().top;
+  var blogPreBlockOffsetTop = $('.pre-block-2').offset().top;
+  var bioPreBlockOffsetTop = $('.pre-block-3').offset().top;
+  var preBlockHeight = $('.pre-block').height();
   var separatorScrollDist = preBlockHeight - 100;
 
   $('.title-container').css({
@@ -11,12 +13,32 @@ $(window).scroll(function(){
     'opacity': (((height) - wScroll) / height)
   });
 
-  if(wScroll > (offsetTop + preBlockHeight * 0.25)){
+  if(wScroll > (skillsPreBlockOffsetTop + preBlockHeight * 0.25)){
     $('.pre-block-1 h1').css({
-      'opacity': (((separatorScrollDist) - (wScroll - offsetTop)) / separatorScrollDist)  
+      'opacity': (((separatorScrollDist) - (wScroll - skillsPreBlockOffsetTop)) / separatorScrollDist)  
     });
   } else {
     $('.pre-block-1 h1').css({
+      'opacity': 1 
+    });
+  }
+
+  if(wScroll > (blogPreBlockOffsetTop + preBlockHeight * 0.25)){
+    $('.pre-block-2 h1').css({
+      'opacity': (((separatorScrollDist) - (wScroll - blogPreBlockOffsetTop)) / separatorScrollDist)  
+    });
+  } else {
+    $('.pre-block-2 h1').css({
+      'opacity': 1 
+    });
+  }
+
+  if(wScroll > (bioPreBlockOffsetTop + preBlockHeight * 0.25)){
+    $('.pre-block-3 h1').css({
+      'opacity': (((separatorScrollDist) - (wScroll - bioPreBlockOffsetTop)) / separatorScrollDist)  
+    });
+  } else {
+    $('.pre-block-3 h1').css({
       'opacity': 1 
     });
   }
