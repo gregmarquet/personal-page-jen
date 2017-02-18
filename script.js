f0aa82d41bff210ffbb2a24eb12cf571658fb414
gregmarquet <marquet.greg@gmail.com>
2017-02-18 14:13:57 -0500

    add js to fade pre-block h1 when scrolled

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
   var height = $(window).height();
 
+  var offsetTop = $('.pre-block-1').offset().top;
+  var preBlockHeight = $('.pre-block-1').height();
+  var separatorScrollDist = preBlockHeight - 100;
+
   $('.title-container').css({
     'transform': 'translate(0px, '+ wScroll / 2.3 +'%)',
-    'opacity': (((height - 150) - wScroll) / height)
+    'opacity': (((height) - wScroll) / height)
   });
+
+  if(wScroll > (offsetTop + preBlockHeight * 0.25)){
+    $('.pre-block-1 h1').css({
+      'opacity': (((separatorScrollDist) - (wScroll - offsetTop)) / separatorScrollDist)  
+    });
+  } else {
+    $('.pre-block-1 h1').css({
+      'opacity': 1 
+    });
+  }
 
 });
 
