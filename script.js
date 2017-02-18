17c4f450e31eddd89887acff14a8f8f9e1648cef
gregmarquet <marquet.greg@gmail.com>
2017-02-18 16:46:34 -0500

    change some var name in script.js and add commants

--- script.js
+++ script.js
@@ -6,15 +6,20 @@ $(window).scroll(function(){
   var blogPreBlockOffsetTop = $('.pre-block-2').offset().top;
   var bioPreBlockOffsetTop = $('.pre-block-3').offset().top;
   var preBlockHeight = $('.pre-block').height();
+
+  // reduce the scrolling distance in the pre-block in order to increase scrolling speed
   var separatorScrollDist = preBlockHeight - 100;
 
+  // create parallax scroll(different speed) and fade title when scrolling
   $('.title-container').css({
     'transform': 'translate(0px, '+ wScroll / 2.3 +'%)',
     'opacity': (((height) - wScroll) / height)
   });
 
+  // set condition to start the scrolling effect 25% into the preblock
   if(wScroll > (skillsPreBlockOffsetTop + preBlockHeight * 0.25)){
     $('.pre-block-1 h1').css({
+      // fade out pre-block h1 when scrolling
       'opacity': (((separatorScrollDist) - (wScroll - skillsPreBlockOffsetTop)) / separatorScrollDist)  
     });
   } else {
